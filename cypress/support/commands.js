5de5ad1d0c3d287ebf657ab10c58d6e81450d96f
Hiroaki Machida <hiroakimachida@dhcp-wifi-8021x-155-41-89-8.bu.edu>
2020-02-27 17:54:12 -0500

    minor change for integration test

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -4,18 +4,8 @@ import 'firebase/database';
 import 'firebase/firestore';
 import { attachCustomCommands } from 'cypress-firebase';
 import 'cypress-file-upload';
+import * as config from '../../src/config'
 
-
-const fbConfig = {
-  apiKey: "AIzaSyDQ4kXM-H_M4y5LabXQRGnEGOSEjHaB0DQ",
-  authDomain: "team1-test-67251.firebaseapp.com",
-  databaseURL: "https://team1-test-67251.firebaseio.com",
-  projectId: "team1-test-67251",
-  storageBucket: "team1-test-67251.appspot.com",
-  messagingSenderId: "374277221605",
-  appId: "1:374277221605:web:d0c37b704a1d48dd90780c"
-};
-
-firebase.initializeApp(fbConfig);
+firebase.initializeApp(config.firebase);
 
 attachCustomCommands({ Cypress, cy, firebase })
