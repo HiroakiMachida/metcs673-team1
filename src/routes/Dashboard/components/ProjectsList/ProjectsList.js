8e095f5ff8c3c3dd10d5a637f3e7707ae22cd99f
Hiroaki Machida <hiroakimachida@Hiroaki-no-MacBook.local>
2020-03-05 12:46:33 -0500

    surpress warning

--- src/routes/Dashboard/components/ProjectsList/ProjectsList.js
+++ src/routes/Dashboard/components/ProjectsList/ProjectsList.js
@@ -3,7 +3,6 @@ import { makeStyles } from '@material-ui/core/styles'
 import styles from './ProjectsList.styles'
 import SellingList from '../SellingList'
 import BuyingList from '../BuyingList'
-import Autocomplete from '@material-ui/lab/Autocomplete';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 
